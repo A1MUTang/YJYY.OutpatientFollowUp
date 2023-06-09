342d338aa73e283325676f4ed44b3629d06b8f3a
Junwen Tang <junwen.tang@cn.abb.com>
2023-06-09 10:33:42 +0800

    feat:添加发送短信处理类

---
+++ OutPatientFollowUp/OutPatientFollowUp.Application/System/Services/User/SMShandle.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using Serilog;
+using ServiceReference1;
+using System.Text.Json;
+namespace CM.API
+{
+    public class SMShandle
+    {
+        public static Service1SoapClient service = new Service1SoapClient(Service1SoapClient.EndpointConfiguration.Service1Soap);
+
+        /// <summary>
+        /// 取得一个6位的随机码
+        public static string GetRandomCode()
+        {
+            //return Guid.NewGuid().ToString().Substring(0, 4);
+
+            Random rd = new Random();
+            int Code = rd.Next(100000, 1000000);
+            return Code.ToString(); ;
+        }
+        public void SendSM(string tel, string data)
+        {
+            //Common.IsLogin(this);
+
+            //CommonFunction.update_data();
+
+            //var msm = new ServiceReference1.Service1SoapClient();
+            /*
+            账号:dlzhuoxin
+            密码:QrH7hr6T   
+            产品信息类型 1012808 
+            */
+            CSubmitState state = service.g_SubmitAsync("dlzhuoxin", "QrH7hr6T", "", "1012808", tel, data + "【社区健康管理平台】").Result;
+            string str = state.MsgID + ":" + state.MsgState;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tel"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool APPSendSM(string tel, string data)
+        {
+            /*
+            账号:dlzhuoxin
+            密码:QrH7hr6T   
+            产品信息类型 1012808 
+            */
+            CSubmitState state = service.g_SubmitAsync("dlzhuoxin", "QrH7hr6T", "", "1012808", tel, data + "【医家医】").Result;
+            //string str = state?.MsgID + ":" + state?.MsgState;
+            //string path = HttpContext.Current.Request.PhysicalApplicationPath + "\\logs";
+            //string filename = path + "/sendMsg.txt";//用日期对日志文件命名
+            Log.Information(DateTime.Now.ToString("HH:mm:ss") + "手机号" + tel + " data" + data + "HMSendSM：" + JsonConvert.SerializeObject(state));
+            if (state == null || state?.MsgID == "0")
+            {
+                return false;
+            }           
+            return true;
+        }
+    }
+}
